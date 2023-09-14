8c981972234ec163879c76e3fbbb1934e3d7a734
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-09-15 00:33:27 +0700

    post

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -6,9 +6,11 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function Post() {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [photo, setPhoto] = useState('')
   const [msg, setMsg] = useState('')
+  // console.log(localStorage.getItem('user'))
 
   const navigate = useNavigate()
 
@@ -16,8 +18,10 @@ function Post() {
     e.preventDefault()
     try {
       await axios.post('http://localhost:3000/user/login', {
-        USERNAME: username,
-        PASSWORD: password,
+        TITLE: title,
+        DESCRIPTION: description,
+        PHOTO: photo,
+        USERS: localStorage.getItem('user'),
       })
       .then((res) => {
         const response = res.data.loginResponse
@@ -44,17 +48,17 @@ function Post() {
       <div className="container d-flex justify-content-start align-items-start mt-lg-5 p-lg-3 bg-success bg-opacity-50" style={{ width: '1280px', height: '530px'}}>
     <Form style={{ width: '500px' }} onSubmit={login}>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicTitle">
         <Form.Label>Title</Form.Label>
-        <Form.Control value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Enter Title..." />
+        <Form.Control value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Enter Title..." />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="formBasicDescription">
         <Form.Label>Description</Form.Label>
-        <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Description..." />
+        <Form.Control value={description} onChange={(e) => setDescription(e.target.value)} type="password" placeholder="Enter Description..." />
       </Form.Group>
       
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="formBasicImages">
         <Form.Label>Images</Form.Label>
         <Form.Control value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter URL..." />
       </Form.Group>
