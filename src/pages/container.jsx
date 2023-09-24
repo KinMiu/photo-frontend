6f472c4349cc98a3342e600b9ac755e03c22b62f
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-09-24 12:29:15 +0700

    update 24-09-23 container update

--- src/pages/container.jsx
+++ src/pages/container.jsx
@@ -5,7 +5,7 @@ const Container = () => {
     const isLogin = !!localStorage.getItem('user')
     return (
         <div className="mt-md-3 flu ">
-            <div className="CardGroup d-flex flex-row gap-2 justify-content-start align-items-center mx-3 flex-wrap border border-2 rounded p-2">
+            <div className="CardGroup d-flex flex-row gap-2 align-items-center mx-3 flex-wrap border border-2 rounded p-2">
                 <Card />
                 {isLogin ? (
                         <></>
