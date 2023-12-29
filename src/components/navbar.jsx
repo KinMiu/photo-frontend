fb2a0145b34931d3b9b60250e6dfb55b168a8035
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-12-29 19:04:58 +0700

    update 19.05 29 des 2023

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function NavScrollExample() {
-    const isLogin = !!localStorage.getItem('user')
+    const isLogin = localStorage.getItem('user')
     console.log(isLogin)
 
     const [name, setName] = useState('')
