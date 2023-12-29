389cf2235be1bec7422495ba0b31ed1440617da6
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-12-29 21:29:38 +0700

    update 21:30 29 des 2023

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -8,12 +8,17 @@ const Dashboard = () => {
     
     const userData = localStorage.getItem('user')
     const parsingData = JSON.parse(userData)
+
+    console.log(parsingData)
+
     const [card, setCard] = useState([])
     useEffect(() => {
-        axios.get(`https://photo-backend.vercel.app/photos/getbyuser/${parsingData.IDUSER}`)
+        axios.get(`https://photo-backend.vercel.app/photos/getbyuser/${parsingData}`)
         .then((res) => {
             setCard(res.data.data)
-        })
+        }).catch(err => {
+            console.error(err)
+        }) 
     }, [])
     return(
         <div className="mt-md-3 flu ">
