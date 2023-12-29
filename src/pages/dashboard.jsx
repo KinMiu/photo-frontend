8860dcd9b89d65c6965025ec8eb5e6432ba6dc72
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-12-29 20:23:41 +0700

    update 19.24 29 des 2023

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,28 +1,27 @@
-import CardUser from "../components/CardUser"
 import Button from 'react-bootstrap/Button';
 
-import axios from 'axios'
-import { useEffect, useState } from "react";
+// import axios from 'axios'
+// import { useEffect, useState } from "react";
 
 const Dashboard = () => {
     
-    const userData = localStorage.getItem('user')
-    const parsingData = JSON.parse(userData)
-    const [card, setCard] = useState([])
-    useEffect(() => {
-        axios.get(`https://photo-backend.vercel.app/photos/getbyuser/${parsingData.IDUSER}`)
-        .then((res) => {
-            // console.log(config.headers.authorization)
-            setCard(res.data.data)
-        })
-    }, [])
+    // const userData = localStorage.getItem('user')
+    // const parsingData = JSON.parse(userData)
+    // const [card, setCard] = useState([])
+    // useEffect(() => {
+    //     axios.get(`https://photo-backend.vercel.app/photos/getbyuser/${parsingData.IDUSER}`)
+    //     .then((res) => {
+    //         // console.log(config.headers.authorization)
+    //         setCard(res.data.data)
+    //     })
+    // }, [])
     return(
         <div className="mt-md-3 flu ">
             <Button href="/post" className="ms-3" variant="success" type="submit">
                 Upload
             </Button>
             <div className="CardGroup d-flex flex-row gap-2 justify-content-start mx-3 flex-wrap mt-3 border rounded border-2 p-2" style={{ minHeight: '400px' }}>
-                <CardUser cardData={card} />
+                {/* <CardUser cardData={card} /> */}
             </div>
         </div>
     )
